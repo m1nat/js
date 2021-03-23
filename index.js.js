f5687b9f50ddfe4f4925b2590a3a178d5367a22a
Vitaliy <Mushpakov.Vitaliy@gmail.com>
2021-03-23 19:01:39 +0300

    name

---
+++ index.js.js
@@ -0,0 +1,64 @@
+// homework_1
+
+// let min = 56;
+
+// if (min <= 15) {
+//     console.log(`firstPart`);
+// } else if (min<= 30) {
+//     console.log(`secondPart`);
+// } else if (min <= 45) {
+//     console.log(`thirdPart`);
+// } else if (min <= 59) {
+//     console.log(`fourthPart`);
+// }
+
+
+// _______________________homework_2_______________________________ 
+
+
+// let num = 3;
+
+// if (num == 1) {
+//     let result = `winter`;
+//     console.log(result);
+// } else if (num == 2) {
+//     let result = `spring`;
+//     console.log(result);
+// } else if (num == 3) {
+//     let result = `summer`;
+//     console.log(result);
+// } else if (num == 4) {
+//     let result = `autumn`;
+//     console.log(result);
+// }
+
+// switch (num) {
+//   case 1:
+//     result = `winter`;
+//     console.log(result);
+//     break;
+//   case 2:
+//     result = `spring`;
+//     console.log(result);
+//     break;
+//   case 3:
+//     result = `summer`
+//     console.log(result);
+//     break;
+//   case 4:
+//     result = `autumn`
+//     console.log(result);
+//     break;
+//   default:
+//     console.log('...');
+//     break;
+// }
+
+// __________________Home_work_3_____________________
+ 
+// let x = 0;
+
+// for (let i = 0; i < 100; i++) {
+//   x+=i;
+// }
+// console.log(x);
